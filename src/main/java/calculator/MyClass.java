82fef9e0380738681188d277bc2aebf9c11fc392
Rosita <sita81@hotmail.com>
2025-04-28 11:38:55 +0200

    Update add and created sub methods.

--- src/main/java/calculator/MyClass.java
+++ src/main/java/calculator/MyClass.java
@@ -1,5 +1,6 @@
 package calculator;
 
 public class MyClass {
-    public int add(int a, int b){ return a + b + 3; }
+    public int add(int a, int b){ return a + b; }
+    public int sub(int a, int b){ return a - b; }
 }
